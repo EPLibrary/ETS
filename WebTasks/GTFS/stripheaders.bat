19061901eb71257e2d7ec2ac047c53d3c2598710
JD Lien <jd@jdlien.com>
2018-05-09 09:18:39 -0600

    Moved from C: to D:

--- WebTasks/GTFS/stripheaders.bat
+++ WebTasks/GTFS/stripheaders.bat
@@ -1,16 +1,16 @@
 @echo off
-C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" C:\inetpub\temp\gtfs\stop_times.txt > C:\inetpub\temp\gtfs\stop_times_noheader.txt
+C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" D:\inetpub\temp\gtfs\stop_times.txt > D:\inetpub\temp\gtfs\stop_times_noheader.txt
 
-C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" C:\inetpub\temp\gtfs\shapes.txt > C:\inetpub\temp\gtfs\shapes_noheader.txt
+C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" D:\inetpub\temp\gtfs\shapes.txt > D:\inetpub\temp\gtfs\shapes_noheader.txt
 
-C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" C:\inetpub\temp\gtfs\trips.txt > C:\inetpub\temp\gtfs\trips_noheader.txt
+C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" D:\inetpub\temp\gtfs\trips.txt > D:\inetpub\temp\gtfs\trips_noheader.txt
 
-C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" C:\inetpub\temp\gtfs\calendar_dates.txt > C:\inetpub\temp\gtfs\calendar_dates_noheader.txt
+C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" D:\inetpub\temp\gtfs\calendar_dates.txt > D:\inetpub\temp\gtfs\calendar_dates_noheader.txt
 
-C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" C:\inetpub\temp\gtfs\stops.txt > C:\inetpub\temp\gtfs\stops_noheader.txt
+C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" D:\inetpub\temp\gtfs\stops.txt > D:\inetpub\temp\gtfs\stops_noheader.txt
 
-C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" C:\inetpub\temp\gtfs\transfers.txt > C:\inetpub\temp\gtfs\transfers_noheader.txt
+C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" D:\inetpub\temp\gtfs\transfers.txt > D:\inetpub\temp\gtfs\transfers_noheader.txt
 
-C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" C:\inetpub\temp\gtfs\routes.txt > C:\inetpub\temp\gtfs\routes_noheader.txt
+C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" D:\inetpub\temp\gtfs\routes.txt > D:\inetpub\temp\gtfs\routes_noheader.txt
 
-C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" C:\inetpub\temp\gtfs\agency.txt > C:\inetpub\temp\gtfs\agency_noheader.txt
+C:\Windows\System32\findstr.exe /V /R "^[a-z].*[a-z]$" D:\inetpub\temp\gtfs\agency.txt > D:\inetpub\temp\gtfs\agency_noheader.txt
